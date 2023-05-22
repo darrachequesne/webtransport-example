46a90982152afa0e171678f32050f465b65ee163
Damien Arrachequesne <damien.arrachequesne@gmail.com>
2023-05-22 14:35:21 +0200

    use an alias to reach the local server

--- generate_cert.sh
+++ generate_cert.sh
@@ -3,4 +3,4 @@ openssl req -new -x509 -nodes \
     -newkey ec -pkeyopt ec_paramgen_curve:prime256v1 \
     -out cert.pem -keyout key.pem \
     -days 10 \
-    -subj '/CN=localhost'
+    -subj '/CN=example.com'
